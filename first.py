3a0440ee13d2d78aee9859d25e7281a88d605960
Jayesh462 <96970253+Jayesh462@users.noreply.github.com>
2025-04-25 17:48:56 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+p=input("Enter A Number: ")
+p=float(p)
+r=8*p
+print(r)
+#Celsius----->> Farenhiet
+'''c=input("Enter a number: ")
+c=float(c)
+f=(c*9/5)+32
+print(f))'''
+
+'''f=input("Enter A Number")
+f=float(f)
+c= (f-32)*5/9
+print(c)'''
